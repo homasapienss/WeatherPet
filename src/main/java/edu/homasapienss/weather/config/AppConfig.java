c1502e72d975a1dc8db47488b1c55944924cb640
Максим <homa19222@gmail.com>
2025-10-11 11:52:26 +0300

    commit Настройка бинов для работы с БД, основные(dataSource, LocalSessionFactoryBean и HibernateTransactionManager для управления транзациями)

--- src/main/java/edu/homasapienss/weather/config/AppConfig.java
+++ src/main/java/edu/homasapienss/weather/config/AppConfig.java
@@ -1,12 +1,78 @@
 package edu.homasapienss.weather.config;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.Properties;
 
 @Configuration
+@EnableTransactionManagement
+@PropertySource("classpath:application.properties")
 @ComponentScan(basePackages = {
         "edu.homasapienss.weather.repositories",
         "edu.homasapienss.weather.services"})
 public class AppConfig {
+    @Value("${db.driver}")
+    private String driver;
 
+    @Value("${db.url}")
+    private String url;
+
+    @Value("${db.username}")
+    private String username;
+
+    @Value("${db.password}")
+    private String password;
+
+    @Value("${hibernate.dialect}")
+    private String dialect;
+
+    @Value("${hibernate.hbm2ddl.auto}")
+    private String ddlAuto;
+
+    @Value("${hibernate.show_sql}")
+    private String showSql;
+
+    @Bean
+    public DataSource dataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setDriverClassName(driver);
+        config.setJdbcUrl(url);
+        config.setUsername(username);
+        config.setPassword(password);
+
+        return new HikariDataSource(config);
+    }
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory(DataSource dataSource) {
+        LocalSessionFactoryBean factory = new LocalSessionFactoryBean();
+        factory.setDataSource(dataSource);
+        factory.setPackagesToScan("edu.homasapienss.weather.models");
+
+        Properties props = new Properties();
+        props.setProperty("hibernate.dialect", dialect);
+        props.setProperty("hibernate.hbm2ddl.auto", ddlAuto);
+        props.setProperty("hibernate.show_sql", showSql);
+
+        factory.setHibernateProperties(props);
+        return factory;
+    }
+
+    @Bean
+    public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
+        HibernateTransactionManager txManager = new HibernateTransactionManager();
+        txManager.setSessionFactory(sessionFactory);
+        return txManager;
+    }
 }
