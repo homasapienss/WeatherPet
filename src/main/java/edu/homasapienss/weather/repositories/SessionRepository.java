0c9412f73b25fa30dc4c6293d5a2b9e214c2aec5
Максим <homa19222@gmail.com>
2025-10-14 15:44:59 +0300

    написал SessionRepository.java

--- src/main/java/edu/homasapienss/weather/repositories/SessionRepository.java
+++ src/main/java/edu/homasapienss/weather/repositories/SessionRepository.java
@@ -1,4 +1,50 @@
 package edu.homasapienss.weather.repositories;
 
-public class SessionRepository {
+import edu.homasapienss.weather.models.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public class SessionRepository implements CrudRepository<Session, UUID> {
+
+    private final SessionFactory sessionFactory;
+
+    @Autowired
+    public SessionRepository(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Override
+    public List<Session> getAll() {
+        return sessionFactory
+                .getCurrentSession()
+                .createQuery("FROM Session", Session.class)
+                .list();
+    }
+
+    @Override
+    public Optional<Session> getById(UUID uuid) {
+        return Optional.ofNullable(sessionFactory
+                .getCurrentSession()
+                .get(Session.class, uuid));
+    }
+
+    @Override
+    public Session saveOrUpdate(Session entityToSave) {
+        return sessionFactory
+                .getCurrentSession()
+                .merge(entityToSave);
+    }
+
+    @Override
+    public void delete(Session entityToDelete) {
+        sessionFactory
+                .getCurrentSession()
+                .remove(entityToDelete);
+    }
 }
