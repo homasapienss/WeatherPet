bbc244153ca343955b6ba8bbb95436d6c3df5067
Максим <homa19222@gmail.com>
2025-10-14 15:48:42 +0300

    написал LocationRepository.java

--- src/main/java/edu/homasapienss/weather/repositories/LocationRepository.java
+++ src/main/java/edu/homasapienss/weather/repositories/LocationRepository.java
@@ -1,4 +1,49 @@
 package edu.homasapienss.weather.repositories;
 
-public class LocationRepository {
+import edu.homasapienss.weather.models.Location;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class LocationRepository implements CrudRepository<Location, Long> {
+
+    @Autowired
+    private final SessionFactory sessionFactory;
+
+    public LocationRepository(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Override
+    public List<Location> getAll() {
+        return sessionFactory
+                .getCurrentSession()
+                .createQuery("FROM Location", Location.class)
+                .list();
+    }
+
+    @Override
+    public Optional<Location> getById(Long aLong) {
+        return Optional.ofNullable(sessionFactory
+                .getCurrentSession()
+                .get(Location.class, aLong));
+    }
+
+    @Override
+    public Location saveOrUpdate(Location entityToSave) {
+        return sessionFactory
+                .getCurrentSession()
+                .merge(entityToSave);
+    }
+
+    @Override
+    public void delete(Location entityToDelete) {
+        sessionFactory
+                .getCurrentSession()
+                .remove(entityToDelete);
+    }
 }
