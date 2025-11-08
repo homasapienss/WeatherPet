11c1de1eee7f28e7e4dd221d279879f1df808e99
Максим <homa19222@gmail.com>
2025-11-08 14:48:16 +0300

    избавился от лишних страниц

--- src/main/java/edu/homasapienss/weather/exceptions/GlobalExceptionHandler.java
+++ src/main/java/edu/homasapienss/weather/exceptions/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package edu.homasapienss.weather.exceptions;
 
+import edu.homasapienss.weather.dto.UserDto;
 import edu.homasapienss.weather.exceptions.auth.LoginException;
 import edu.homasapienss.weather.exceptions.auth.RegisterException;
 import org.springframework.ui.Model;
@@ -20,16 +21,17 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(LoginException.class)
     public String handleLoginError(LoginException ex, Model model) {
         addErrorToPage(ex, model);
-        return "login-error";
+        return "login";
     }
 
     @ExceptionHandler(RegisterException.class)
     public String handleRegisterError(RegisterException ex, Model model) {
         addErrorToPage(ex, model);
-        return "register-error";
+        return "register";
     }
 
     private void addErrorToPage (Exception ex, Model model) {
+        model.addAttribute("userDto", new UserDto("", ""));
         model.addAttribute(ERROR_ATTRIBUTE, ex.getMessage());
     }
 }
